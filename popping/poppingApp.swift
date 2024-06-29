f0c018ac071ed7fa161b47d23e1266007b2fb463
Neopuyo <neo.outnumber275@passmail.com>
2024-06-29 10:08:24 +0200

    Initial Commit

---
+++ popping/poppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  poppingApp.swift
+//  popping
+//
+//  Created by Loup Martineau on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct poppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
